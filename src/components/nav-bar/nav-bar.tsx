60a02a9bb16e9219791f561ca10f555506beb975
robMolloy <robmolloy@hotmail.co.uk>
2024-02-27 11:25:38 +0000

    feat/add-pointless-nav-tab-2

--- src/components/nav-bar/nav-bar.tsx
+++ src/components/nav-bar/nav-bar.tsx
@@ -59,6 +59,9 @@ export class NavBar {
                   </li>
                 )}
                 <li>
+                  <a>whatever 2</a>
+                </li>
+                <li>
                   <div class="dropdown dropdown-bottom dropdown-end">
                     <div tabindex="0">Theme</div>
                     <ul
